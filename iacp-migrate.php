42d4d90a23fd00c2ad53b346ce9025d391a8689a
Ian Betancourt <ibetancourt@matrixgroup.net>
2016-08-31 11:44:20 -0400

    Moved query to its own file, Correctly gets and formats the data, Now just needs to actually give that data to WordPress.

--- iacp-migrate.php
+++ iacp-migrate.php
@@ -13,11 +13,15 @@
 */
 
 /* OPTIONS */
+/* These are all called "Debug" not because they do any debugging on their own,
+ * but rather because you should use them while debugging.
+ */
 define("DEBUG", true);           /* Set to false to disable all other DEBUGs. */
 define("DEBUG_CREDS", DEBUG && false);        /* Echo credentials information */
 define("DEBUG_DATA", DEBUG && false);         /* Echo all exported data. */
 define("DEBUG_FIRST_LINE", DEBUG && false);   /* Echo first line information. */
-define("DEBUG_QUERY", DEBUG && true);         /* Echo query information. */
+define("DEBUG_QUERY", DEBUG && true);        /* Echo complete query. */
+define("DEBUG_QUERY_RESULTS", DEBUG && false);/* Echo the query results. */
 define("USE_CONNECTION_POOLING", false); /* Make a new connection every time? */
 define("USE_WINDOWS_AUTH", true);       /* Use Windows Auth to connect to db? */
 
@@ -45,9 +49,10 @@ if(USE_CONNECTION_POOLING || USE_WINDOWS_AUTH){
 }
 
 /* CONSTANTS */
-define("FIELD_REQUIRED", true);
-define("FIELD_NOT_REQUIRED", false);
 define("COLUMN_NOT_FOUND", -1);
+define("DEBUG_LINE_WIDTH", 80);
+define("FIELD_NOT_REQUIRED", false);
+define("FIELD_REQUIRED", true);
 
 /* This class is basically just a struct for holding information about field
  * names and where they are.
@@ -68,56 +73,21 @@ class IACPField {
 /* Every field in this array is one that will be mapped to a field in the new
  * database. If any of the fields still have the default value after the file
  * has been read, the program should fail.
+ *
+ * The first parameter is the name of a field in the database we're exporting
+ * from. The second is that same field in the database we're importing to. If
+ * you want two fields to become one, change the query to combine them first.
  */
 $iacp_fields_array = [
-    new IACPField("article_title", "", FIELD_REQUIRED),
-    new IACPField("article_text", "", FIELD_REQUIRED),
-    new IACPField("issue_month", "", FIELD_REQUIRED),
-    new IACPField("issue_year", "", FIELD_REQUIRED),
-    new IACPField("issue_description", "", FIELD_REQUIRED),
-    new IACPField("volume", "", FIELD_REQUIRED),
-    new IACPField("issue_id", "", FIELD_REQUIRED),
-    new IACPField("cover_filename", "", FIELD_REQUIRED),
+    new IACPField("article_id", "", FIELD_REQUIRED),
+    new IACPField("article_title", "", FIELD_NOT_REQUIRED),
+    new IACPField("article_text", "", FIELD_NOT_REQUIRED),
+    new IACPField("issue_date", "", FIELD_NOT_REQUIRED),
+    new IACPField("issue_description", "", FIELD_NOT_REQUIRED),
+    new IACPField("volume", "", FIELD_NOT_REQUIRED),
+    new IACPField("issue_id", "", FIELD_NOT_REQUIRED),
+    new IACPField("cover_filename", "", FIELD_NOT_REQUIRED),
 ];
-
-/* Handles all of reading a given first line of a csv file to figure out where
- * each of our fields are in order.
- */
-function iacp_read_first_line(string $fl){
-    if(empty($fl)){
-        die("First line is empty!");
-    }
-
-    if(DEBUG_FIRST_LINE){
-        echo "Raw first line: " . $fl . PHP_EOL;
-    }
-    
-    //Look at each individual element of the csv
-    foreach (explode(",", $fl) as $index => $fieldname){
-        //Loop through the array of fields
-        foreach((array)$iacp_fields_array as $field){
-            
-            if(DEBUG_FIRST_LINE){
-                echo "Field " . $index . ": " . $fieldname . PHP_EOL;
-            }
-            
-            //And if it's there, set it
-            if(strcmp(strtolower(trim($field->orig_name)),
-                      strtolower(trim($fieldname))) == 0){
-                $field->col_num = $index;
-                
-                break;
-            }
-        }
-    }
-
-    //Now make sure every required field was there
-    foreach((array)$iacp_fields_array as $field){
-        if($field->col_num == FIELD_REQUIRED){
-            die("Didn't find " . $field.orig_name . ".");
-        }
-    }
-}
 
 /* This function should only ever be called after iacp_read_first_line. It
  * expects the col_num field of the 
@@ -148,6 +118,9 @@ function iacp_format_line($line){
     return $to_return;
 }
 
+/* Small little function to generate the second argument for sqlsrv_connect().
+ * It takes into account the options at the top of the file. Returns an array.
+ */
 function iacp_get_connection_info($creds){
     $connectionInfo = [];
     
@@ -163,6 +136,30 @@ function iacp_get_connection_info($creds){
     return $connectionInfo;
 }
 
+/* Returns the query in 'query.txt' to be performed on the database in
+ * 'credentials.txt'.
+ *
+ * Each row of the result should contain all the columns in $iacp_fields_array,
+ * but only the required columns are necessary.
+ */
+function iacp_read_query(){
+    $filename = "query.txt";
+    $f = fopen($filename, "r") or die("Unable to read query file!");
+    $query = "";
+
+    //Just want to turn the whole file into a single string.
+    while(!empty($line = fgets($f))){
+        $query = $query . $line;
+    }
+
+    //Don't forget to close the file!
+    if($f){
+        fclose($f);
+    }
+    
+    return $query;
+}
+
 /* Returns the credentials in the credential file in an associative array.
  */
 function iacp_read_credentials(){
@@ -175,10 +172,11 @@ function iacp_read_credentials(){
     ];
     
     $filename = "credentials.txt";
-    $f = fopen($filename, "r") or die("Unable to open file!");
+    $f = fopen($filename, "r") or die("Unable to read credentials file!");
 
     if(DEBUG_CREDS){echo "Beginning read..." . PHP_EOL;}
 
+    //Run through the file and put each matching line into $to_return
     while(!empty($line = fgets($f))){
         //No need for a PHP_EOL on this echo since $line will contain a newline
         if(DEBUG_CREDS){
@@ -196,6 +194,11 @@ function iacp_read_credentials(){
         }
     }
 
+    //Don't forget to close the file!
+    if($f){
+        fclose($f);
+    }
+    
     //If any of the fields wasn't found, fail
     foreach((array)$to_return as $key => $field){
         if(empty($field)){
@@ -210,7 +213,7 @@ function iacp_read_credentials(){
     return $to_return;
 }
 
-/* We're going to grab the first csv file in this directory and break it up into
+/* We're going to grab the data from the specified database and break it up into
  * something we can use. This function is separate so that if we want to turn it
  * into something that performs operations directly on a csv file, we won't have
  * to change any other functions.
@@ -219,19 +222,71 @@ function iacp_read_credentials(){
  * trying to move.
  */
 function iacp_get_data(){
-    $to_return = [];
+    global $iacp_fields_array;
     $creds = iacp_read_credentials();
-
+    
     //Create the connection information array, taking into account options
     $connectionInfo = iacp_get_connection_info($creds);
 
     // Actually create connection
     echo "Connecting to " . $creds["server"] . "... ";
     $conn = sqlsrv_connect($creds["server"], $connectionInfo) or die("Failed.");
-    echo "Success." . PHP_EOL;
+    echo "Connection opened." . PHP_EOL . PHP_EOL;
 
+    //Perform the query
+    $query = iacp_read_query();
+    if(DEBUG_QUERY){ echo "Query: " . PHP_EOL . $query  . PHP_EOL;}
+    $resp = sqlsrv_query($conn, $query, []) or die("Query failed!");
+
+    //Process the result
+    $to_return = [];
+    while($obj = sqlsrv_fetch_object( $resp )){
+        $line = [];
+
+        /* If a required field is missing from the line, we want to skip adding
+         * the line to $to_return. The easiest way to keep track of these lines
+         * is with a boolean. We set it to false when we fail, and then we just
+         * perform a quick check before adding the line to make sure it is okay
+         * to add to $to_return.
+         */
+        $all_required_fields_accounted_for = true;
+        
+        /* The migration is expecting an array, so we can't just pass back the
+         * object. This also does sort the array that we create into the order
+         * outlined in the iacp_fields_array.
+         */
+        foreach((array)$iacp_fields_array as $field){
+            //Grab the next field and drop it into the array
+            $prop_name = $field->orig_name;
+            $value = $obj->$prop_name;
+            $line[] = $value;
+
+            //Check for missing data from required field.
+            if($field->required == FIELD_REQUIRED && empty($value)){
+                echo "Missing required field! Skipping line.";
+                $all_required_fields_accounted_for = false;
+                break;
+            }
+            
+            if(DEBUG_QUERY_RESULTS){
+                /* Since some fields can be excessively long, restrict them to
+                 * a pre-set, constant length.
+                 */
+                echo str_replace("\n", " ", substr($value, 0, DEBUG_LINE_WIDTH))
+                    . ", ";
+            }
+        }
+
+        if($all_required_fields_accounted_for){
+            $to_return[] = $line;
+        }
+        
+        //Two newlines after to guarantee readability
+        if(DEBUG_QUERY_RESULTS){ echo PHP_EOL . PHP_EOL; }
+    }
+
+    echo count($to_return) . " records imported." . PHP_EOL . PHP_EOL;
     
-
     // Close connection when done.
     if(!empty($conn)){
         echo "Closing connection...  ";
@@ -242,6 +297,8 @@ function iacp_get_data(){
     return $to_return;
 }
 
+/* Performs the data migration.
+ */
 function iacp_migrate(){
     $data = iacp_get_data();
     $counter = 1;
@@ -255,6 +312,7 @@ function iacp_migrate(){
                 echo $field . PHP_EOL;
             }
         }
+        if(DEBUG_DATA){ echo PHP_EOL; }
         $counter++;
     }
 }
